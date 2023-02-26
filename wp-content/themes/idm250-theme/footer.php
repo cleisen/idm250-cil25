22c394dc8bcf58c3f35dfb0da75ae22b0f00c668
Christina Leisen <cil25@drexel.edu>
2023-02-25 19:19:51 -0500

    Dynamic menus

--- wp-content/themes/idm250-theme/footer.php
+++ wp-content/themes/idm250-theme/footer.php
@@ -1,3 +1,17 @@
 <?php wp_footer(); ?>
+
+<footer>
+    <nav class="foot-menu">
+        <?php
+        wp_nav_menu(['theme_location' => 'footer-menu']);
+        ?>
+    </nav>
+    <div class="center">
+        <img src="/images/icons/insta-icon.png" class="insta-icon">
+    </div>
+    <div class="center">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    </div>
+</footer>
 </body>
 </html>
--- wp-content/themes/idm250-theme/header.php
+++ wp-content/themes/idm250-theme/header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+    <link rel="stylesheet" href="https://use.typekit.net/zik2ksl.css">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
 <?php wp_body_open();?>
 
 <header>
-    <p>LOGO GOES HERE</p>
     <nav class="main-menu">
         <?php
         wp_nav_menu(['theme_location' => 'primary-menu']);
