8b29b87ba06817491753af4604e6e3bfd29ed0e6
Christina Leisen <cil25@drexel.edu>
2023-02-25 19:31:58 -0500

    image

--- wp-content/themes/idm250-theme/footer.php
+++ wp-content/themes/idm250-theme/footer.php
@@ -7,7 +7,7 @@
         ?>
     </nav>
     <div class="center">
-        <img src="/images/icons/insta-icon.png" class="insta-icon">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/icons/insta-icon.png" class="insta-icon">
     </div>
     <div class="center">
         <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
