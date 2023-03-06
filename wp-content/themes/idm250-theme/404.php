70056f9f146d435f307ca3756683fd64a33557b3
Christina Leisen <cil25@drexel.edu>
2023-03-05 19:15:49 -0500

    Start single
    404 functions, added images for single page

--- wp-content/themes/idm250-theme/404.php
+++ wp-content/themes/idm250-theme/404.php
@@ -1,4 +1,13 @@
-<h1><?php echo get_the_title(); ?></h1>
+<?php get_header(); ?>
+<h1>404</h1>
+
+<p class="">
+    This page does not exist
+    </p>
+<p class="">
+    Sorry, we couldn&apos;t find the page
+        you&apos;re looking for.
+    </p>
 
 <?php get_template_part('components/content');?>
 
