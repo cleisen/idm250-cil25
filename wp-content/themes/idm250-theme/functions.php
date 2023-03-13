7eb260844975eb4e9eeeb2f45a2010bc28a9daa5
Christina Leisen <cil25@drexel.edu>
2023-03-13 02:35:43 -0400

    acf update

--- wp-content/themes/idm250-theme/functions.php
+++ wp-content/themes/idm250-theme/functions.php
@@ -137,44 +137,28 @@ function register_theme_sidebars()
 }
 add_action('widgets_init', 'register_theme_sidebars');
 
+
+/**
+ * Save ACF fields to JSON in a directory in your theme
+ * @return void
+ */
 function my_acf_json_save_point($path)
 {
-    // update path
-    $path = get_stylesheet_directory() . '/my-custom-folder';
-
-    // return
+    $acf_directory = '/acf';
+    $path = get_stylesheet_directory() . $acf_directory;
     return $path;
 }
 add_filter('acf/settings/save_json', 'my_acf_json_save_point');
 
-
-function my_acf_json_load_point( $paths ) {
-    
-    // remove original path (optional)
+/**
+ * Load ACF fields from JSON in a directory in your theme
+ * @return void
+ */
+function my_acf_json_load_point($paths)
+{
     unset($paths[0]);
-    
-    
-    // append path
-    $paths[] = get_stylesheet_directory() . '/my-custom-folder';
-    
-    
-    // return
+    $acf_directory = '/acf';
+    $paths[] = get_stylesheet_directory() . $acf_directory;
     return $paths;
-    
 }
 add_filter('acf/settings/load_json', 'my_acf_json_load_point');
-
-
-function add_acf_options_page()
-{
-    if (function_exists('acf_add_options_page')) {
-        acf_add_options_page([
-            'page_title' => 'Theme General Settings',
-            'menu_title' => 'Theme Settings',
-            'menu_slug' => 'theme-general-settings',
-            'capability' => 'edit_posts',
-            'redirect' => false,
-        ]);
-    }
-}
-add_action('init', 'add_acf_options_page');
